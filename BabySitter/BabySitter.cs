4a754c441622c898ebb347e155c54a33dc37274d
Michael Komar <mkomar9109@aol.com>
2018-07-11 14:52:04 -0400

    Created tests to validate start times

---
+++ BabySitter/BabySitter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BabySitterNameSpace
+{
+    public class BabySitter
+    {
+        public bool ValidateTimes(string startTime, string endTime, string bedTime)
+        {
+            DateTime start = Convert.ToDateTime(startTime);
+            DateTime end = Convert.ToDateTime(endTime);
+
+            if (start >= Convert.ToDateTime("5:00pm"))
+            {
+                return true;
+            }
+            else if (start < Convert.ToDateTime("5:00pm"))
+                return false;
+
+            return true;
+           
+        }
+    }
+}
--- BabySitterTest/BabySitterUnitTest1.cs
+++ BabySitterTest/BabySitterUnitTest1.cs
@@ -15,5 +15,23 @@ namespace BabySitterTestNameSpace
             bool validTimes = obj.ValidateTimes("5:00pm","12:00am", "9:00pm");
             Assert.AreEqual(true, validTimes);
         }
+
+        [TestMethod]
+        public void ValidateStartTimeIsBefore5PMReturnsTrue()
+        {
+            BabySitter obj = new BabySitter();
+            bool validTimes = obj.ValidateTimes("5:00pm", "12:00am", "9:00pm");
+            Assert.AreEqual(true, validTimes);
+        }
+
+        [TestMethod]
+        public void ValidateStartTimeIsBefore5PMReturnsFalse()
+        {
+            BabySitter obj = new BabySitter();
+            bool validTimes = obj.ValidateTimes("4:00pm", "12:00am", "9:00pm");
+            Assert.AreEqual(false, validTimes);
+        }
+
+        
     }
 }
